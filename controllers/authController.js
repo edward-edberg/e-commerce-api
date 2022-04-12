89f7f464bfd425586954aaa8f5a26305cf381559
Edward Edberg <edward.edberg.halim@gmail.com>
2022-04-12 00:10:20 -0700

    create user, check email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,17 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError } = require("../errors");
+// const { BadRequestError, UnauthenticatedError } = require("../errors");
+const CustomError = require("../errors");
 
 const register = async (req, res) => {
-  res.send("register");
+  const { email } = req.body;
+  const emailAlreadyExists = await User.findOne();
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists.");
+  }
+  const user = await User.create(req.body);
+  // res.send("register");
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
