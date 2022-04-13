e5c579f4ad1189bd028b5171db7b899f54c41473
Edward Edberg <edward.edberg.halim@gmail.com>
2022-04-13 11:52:13 -0700

    add login and logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-// const { BadRequestError, UnauthenticatedError } = require("../errors");
 const CustomError = require("../errors");
-// const jwt = require("jsonwebtoken");
+
 const {
   createJWT,
   isTokenValid,
@@ -15,7 +14,6 @@ const register = async (req, res) => {
   if (emailAlreadyExists) {
     throw new CustomError.BadRequestError("Email already exists.");
   }
-
   // first registered user is an admin
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? "admin" : "user";
@@ -26,11 +24,34 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  // res.send("login");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
-  res.send("logout");
+  // res.send("logout");
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
 
 module.exports = {
