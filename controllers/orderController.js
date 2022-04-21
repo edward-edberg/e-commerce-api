f1a4ef2f01ba266271274f10e7880974170f8006
Edward Edberg <edward.edberg.halim@gmail.com>
2022-04-20 23:45:50 -0700

    add create order

---
+++ controllers/orderController.js
@@ -0,0 +1,74 @@
+const Review = require("../models/Review");
+const Product = require("../models/Product");
+const { checkPermissions } = require("../utils");
+
+const Order = require("../models/Order");
+const StatusCodes = require("http-status-codes");
+const CustomError = require("../errors");
+const path = require("path");
+
+const getAllOrders = async (req, res) => {
+  res.send("get all orders");
+};
+
+const getSingleOrder = async (req, res) => {
+  res.send("get single order");
+};
+
+const getCurrentUserOrders = async (req, res) => {
+  res.send("get current user orders");
+};
+
+const createOrder = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError("No cart items provided");
+  }
+
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      "Please provide tax and shipping fee"
+    );
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(
+        `No product with id: ${item.product}`
+      );
+    }
+    const { name, price, image, _id } = dbProduct;
+    console.log(name, price, image);
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    // add item to order
+    orderItems = [...orderItems, singleOrderItem];
+    // calculate subtotal
+    subtotal += item.amount * price;
+  }
+  console.log(orderItems);
+  console.log(subtotal);
+  res.send("create order");
+};
+
+const updateOrder = async (req, res) => {
+  res.send("update order");
+};
+
+module.exports = {
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
+};
